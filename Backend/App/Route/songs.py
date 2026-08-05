dca7218bac7250933c6b13ccb3d8cdcc12a5091c
K-Anant412 <anantkore412@gmail.com>
2026-08-05 13:42:21 +0530

    creating end point for searching a specific song by id

--- Backend/App/Route/songs.py
+++ Backend/App/Route/songs.py
@@ -6,6 +6,7 @@ import os
 
 song_route = Blueprint("song", __name__)
 
+
 @song_route.route("/get_songs", methods=["GET"])
 def show_all_songs():
     """
@@ -19,20 +20,61 @@ def show_all_songs():
     """
     try:
         raw_list = Songs.query.all()
-        
+
         if not raw_list:
             return error_response(message="Your directory has no songs.")
-        
+
         songs = []
         for song in raw_list:
-            songs.append({
-                "id":song.song_id,
-                "title":song.title,
-                "artist":song.artist,
-                "is_favorite":song.is_favorite
-            })
-            
+            songs.append(
+                {
+                    "id": song.song_id,
+                    "title": song.title,
+                    "artist": song.artist,
+                    "is_favorite": song.is_favorite,
+                }
+            )
+
         return success_response("Track list:", data=songs)
-    
+
+    except Exception as e:
+        return error_response(message=str(e))
+
+
+@song_route.route("/search_by_id/<int:id>", methods=["GET"])
+def search_song_byId(id):
+    """
+    Search song by ID
+    ---
+    tags:
+        - Search song by ID
+    parameters:
+        - in: path
+          name: id
+          type: integer
+          required: true
+          description: Song ID
+    responses:
+        200:
+            description: Song found successfully
+        404:
+            description: Song not found
+    """
+    try:
+        song_data = Songs.query.get(id)
+
+        if not song_data:
+            return error_response("Song not found.")
+
+        song = {
+            "id": song_data.song_id,
+            "title": song_data.title,
+            "artist": song_data.artist,
+            "album": song_data.album,
+            "duration": song_data.duration,
+            "is_favorite": song_data.is_favorite,
+        }
+
+        return success_response("Song found: ", data=song)
     except Exception as e:
         return error_response(message=str(e))

fe49a461d67712ad6c8390de65cecef82684ceeb
K-Anant412 <anantkore412@gmail.com>
2026-08-05 14:16:27 +0530

    creating end point that update the tracks existing data, like title, artist, album

--- Backend/App/Route/songs.py
+++ Backend/App/Route/songs.py
@@ -101,22 +101,84 @@ def search_song_byName(name):
     """
     try:
         song_data = Songs.query.filter_by(title=name).first()
-        
+
         if not song_data:
             return error_response("Song not found.")
-        
+
         song = {
-                    "id": song_data.song_id,
-                    "title": song_data.title,
-                    "artist": song_data.artist,
-                    "album": song_data.album,
-                    "duration": song_data.duration,
-                    "is_favorite": song_data.is_favorite,
-                }
-        
+            "id": song_data.song_id,
+            "title": song_data.title,
+            "artist": song_data.artist,
+            "album": song_data.album,
+            "duration": song_data.duration,
+            "is_favorite": song_data.is_favorite,
+        }
+
         return success_response("Song found: ", data=song)
-        
+
     except Exception as e:
         return error_response(message=str(e))
 
 
+@song_route.route("/update_songs_data/<int:id>", methods=["PUT"])
+def update_song_data(id):
+    """
+    Update Song Information
+    ---
+    tags:
+      - Songs
+
+    parameters:
+      - in: path
+        name: id
+        type: integer
+        required: true
+        description: ID of the song to update
+
+      - in: body
+        name: body
+        required: true
+        schema:
+            type: object
+        properties:
+            title:
+                type: string
+                example: Believer
+            artist:
+                type: string
+                example: Imagine Dragons
+            album:
+                type: string
+                example: Evolve
+
+    responses:
+        200:
+            description: Song updated successfully
+
+        400:
+            description: Invalid request or no fields provided
+
+        404:
+            description: Song not found
+    """
+    try:
+        data = request.get_json()
+        song = Songs.query.get(id)
+
+        if not song:
+            return error_response("Song not found.")
+
+        if data.get("title", "").strip():
+            song.title = data["title"].strip()
+
+        if data.get("artist", "").strip():
+            song.artist = data["artist"].strip()
+
+        if data.get("album", "").strip():
+            song.album = data["album"].strip()
+
+        db.session.commit()
+
+        return success_response(message="Song updated")
+    except Exception as e:
+        return error_response(message=str(e))
